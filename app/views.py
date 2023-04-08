2a1c6a54158eaf8fe5e798baa8e6de57dddc8c78
MaillotCedric <cedricmaillot38@gmail.com>
2023-04-08 18:04:49 +0400

    Update de l'application web

--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+# from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# @login_required(login_url="login")
+def home(request):
+    return render(request, "home.html", {})
+
+def login_(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.warning(request, "Username and/or password not valid")
+            return redirect("login")
+    else:
+        return render(request, "login.html", {})
