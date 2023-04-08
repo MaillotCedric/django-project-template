bbac49a795c758e7a3f48eebbc352f73a886e3ac
MaillotCedric <cedricmaillot38@gmail.com>
2023-04-08 18:53:12 +0400

    Update de l'application web

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("login", views.login_, name="login"),
+    path("", views.login_, name="login"),
+    path("logout", views.logout_, name="logout"),
+    path("home", views.home, name="home"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
-# @login_required(login_url="login")
+@login_required(login_url="login")
 def home(request):
     return render(request, "home.html", {})
 
@@ -14,9 +14,17 @@ def login_(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+
             return redirect("home")
         else:
             messages.warning(request, "Username and/or password not valid")
+
             return redirect("login")
     else:
         return render(request, "login.html", {})
+
+def logout_(request):
+    logout(request)
+    messages.success(request, "Logged out")
+
+    return redirect("login")
